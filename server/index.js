99f6caaec2a89f6a1c3d605128f200459a232f0e
Hieu Ho <hihho@ucdavis.edu>
2020-06-06 09:01:58 -0700

    Installed nodemon

--- server/index.js
+++ server/index.js
@@ -3,5 +3,14 @@ const path = require('path');
 const db = require('../db');
 
 const app = express();
+const port = 4000;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('req: ', req.body);
+  res.end('my dude');
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
